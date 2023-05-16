b9af2f94b02efb42e2c9ca3acb2067ab75275cea
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2023-05-16 14:16:52 +0600

    feat: Add page Favorites

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
-import { MainView } from './views/main/main.js'
+import { MainView } from "./views/main/main.js";
+import { FavoritesView } from "./views/main/favorites/favorites.js";
 
 class App {
-	routes = [
-		{
-			path: '',
-			view: MainView,
-		},
-	]
+  routes = [
+    {
+      path: "",
+      view: MainView,
+    },
+    {
+      path: "#favorites",
+      view: FavoritesView,
+    },
+  ];
 
-	appState = {
-		favorites: [],
-	}
+  appState = {
+    favorites: [],
+  };
 
-	constructor() {
-		window.addEventListener('hashchange', this.route.bind(this))
-		this.route()
-	}
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+    this.route();
+  }
 
-	route() {
-		if (this.currentView) {
-			this.currentView.destroy()
-		}
+  route() {
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
 
-		const view = this.routes.find(r => r.path == location.hash).view
-		this.currentView = new view(this.appState)
-		this.currentView.render()
-	}
+    const view = this.routes.find((r) => r.path == location.hash).view;
+    this.currentView = new view(this.appState);
+    this.currentView.render();
+  }
 }
 
-new App()
+new App();
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -21,9 +21,14 @@ export class MainView extends AbstractView {
     this.setTitle("Поиск книг");
   }
 
+  destroy() {
+    onChange.unsubscribe(this.appState)
+    onChange.unsubscribe(this.state)
+  }
+
   appStateHook(path) {
     if (path === "favorites") {
-      this.render()
+      this.render();
     }
   }
 
@@ -35,7 +40,7 @@ export class MainView extends AbstractView {
         this.state.offset
       );
       this.state.loading = false;
-      this.state.numFound = data?.numFound
+      this.state.numFound = data?.numFound;
       this.state.list = data?.docs;
     }
 
@@ -53,6 +58,7 @@ export class MainView extends AbstractView {
 
   render() {
     const main = document.createElement("div");
+    main.innerHTML = `<h1>Найдено книг - ${this.state.numFound}</h1>`;
     main.append(new Search(this.state).render());
     main.append(new CardList(this.appState, this.state).render());
     this.app.innerHTML = "";
